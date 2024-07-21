c950f06dc9f9ddc96eede61ab912fab084f0e487
Kai <kaiximenes@outlook.com>
2024-07-21 17:54:17 -0300

    Inclusão do Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,50 @@
 const Footer = () => {
     return (
         <>
-        
+        <div class="footer">
+            <section className="secao-1">
+                <h1>Digital Store</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel vitae autem, a nam est incidunt ab magni, exercitationem iure debitis fuga sed veritatis reiciendis sit possimus rerum. Voluptas, totam vitae!</p>
+            </section>
+            <section className="secao-2">
+                <nav>
+                    <ul><h2>Informação</h2>
+                        <li>Sobre Drip Store</li>
+                        <li>Segurança</li>
+                        <li>Wishlist</li>
+                        <li>Blog</li>
+                        <li>Trabalhe conosco</li>
+                        <li>Meus Pedidos</li>
+                    </ul>
+                </nav>
+            </section>
+            <section className="secao-3">
+                <nav>
+                    <ul><h2>Informação</h2>
+                        <li>Sobre Drip Store</li>
+                        <li>Segurança</li>
+                        <li>Wishlist</li>
+                        <li>Blog</li>
+                        <li>Trabalhe conosco</li>
+                        <li>Meus Pedidos</li>
+                    </ul>
+                </nav>
+            </section>
+            <section className="secao-4">
+                <nav>
+                    <ul><h2>Informação</h2>
+                        <li>Sobre Drip Store</li>
+                        <li>Segurança</li>
+                        <li>Wishlist</li>
+                        <li>Blog</li>
+                        <li>Trabalhe conosco</li>
+                        <li>Meus Pedidos</li>
+                    </ul>
+                </nav>
+            </section>
+            
+
+        </div>
         </>
     );
 }
