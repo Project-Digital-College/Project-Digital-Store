52acfd6c23687e4a6823433306a719d9cb178515
Kai <kaiximenes@outlook.com>
2024-07-21 18:13:51 -0300

    Alterações no Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,13 +2,13 @@ const Footer = () => {
     return (
         <>
         <div class="footer">
-            <section className="secao-1">
+            <section className="footer-secao-1">
                 <h1>Digital Store</h1>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel vitae autem, a nam est incidunt ab magni, exercitationem iure debitis fuga sed veritatis reiciendis sit possimus rerum. Voluptas, totam vitae!</p>
+                <p className="footer-paragrafo">Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel vitae autem, a nam est incidunt ab magni, exercitationem iure debitis fuga sed veritatis reiciendis sit possimus rerum. Voluptas, totam vitae!</p>
             </section>
-            <section className="secao-2">
+            <section className="footer-secao-2">
                 <nav>
-                    <ul><h2>Informação</h2>
+                    <ul><h4>Informação</h4>
                         <li>Sobre Drip Store</li>
                         <li>Segurança</li>
                         <li>Wishlist</li>
@@ -18,29 +18,23 @@ const Footer = () => {
                     </ul>
                 </nav>
             </section>
-            <section className="secao-3">
+            <section className="footer-secao-3">
                 <nav>
-                    <ul><h2>Informação</h2>
-                        <li>Sobre Drip Store</li>
-                        <li>Segurança</li>
-                        <li>Wishlist</li>
-                        <li>Blog</li>
-                        <li>Trabalhe conosco</li>
-                        <li>Meus Pedidos</li>
+                    <ul><h4>Categorias</h4>
+                        <li>Camisetas</li>
+                        <li>Calças</li>
+                        <li>Bonés</li>
+                        <li>Headphones</li>
+                        <li>Tênis</li>
                     </ul>
                 </nav>
             </section>
-            <section className="secao-4">
-                <nav>
-                    <ul><h2>Informação</h2>
-                        <li>Sobre Drip Store</li>
-                        <li>Segurança</li>
-                        <li>Wishlist</li>
-                        <li>Blog</li>
-                        <li>Trabalhe conosco</li>
-                        <li>Meus Pedidos</li>
-                    </ul>
-                </nav>
+            <section className="footer-secao-4">
+                    <h4>Contato</h4>
+                    <p>Av. Santos Dumont, 1510 - 1 andar - Aldeota, Fortaleza - CE, 60150-161</p>
+                    <p>(85) 3051-3411</p>
+
+                        
             </section>
             
 
