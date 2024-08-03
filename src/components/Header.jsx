dfd5ddb786f0706ddaaaca18891f4b9c520c4cd6
Kaiky Xavier <kaikyxavier59@gmail.com>
2024-08-03 12:47:36 -0300

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,31 +1,39 @@
+import 'primeicons/primeicons.css';
+import logo from "../../imagens/logo.png";
+
 const Header = () => {
     return (
         <>
         <div>
+
             <div>
-                <section class="header-superior">
-                    <img class="logo" src="https://media.licdn.com/dms/image/C4E0BAQGOBx_Tgf4xFA/company-logo_200_200/0/1656456553386/digitalcollegebr_logo?e=1729123200&v=beta&t=EP6-vkiEaIXSiL3cSbfsVWsfPwSDgUp7JnVvtk-nuks" alt="Digital College Logo" />
-                    <h1 class="title-header" >Digital Store</h1>
-                    <input type="text" class="input-busca" placeholder="Pesquise seu produto..." />
-                    <a class="link-cadastro" href="" >Cadastre-se</a>
-                    <button class="botao-entrar">Entrar</button> 
+                <section className="header-superior">
+                    <img className="logo" src={logo} alt="Digital College Logo" />
+                    <input type="text" className="input-busca" placeholder="Pesquise seu produto..." />
+                    <a className="link-cadastro" href="" >Cadastre-se</a>
+                    <button className="botao-entrar">Entrar</button>
+                    <i className="pi pi-shopping-cart" style={{ fontSize: '1.25rem', color: '#C92071' }}></i>
                 </section>
 
             </div>
             <div>
-                <nav class="header-bar">
-                    <ul class="bar">
-                        <li>Home</li>
-                        <li>Produtos</li>
-                        <li>Categorias</li>
-                        <li>Meus Pedidos</li>
+                <nav>
+                    <ul className="bar">
+                        <li className="bar-1" >Home</li>
+                        <li className="bar-2" >Produtos</li>
+                        <li className="bar-3" >Categorias</li>
+                        <li className="bar-4" >Meus Pedidos</li>
                     </ul>
                 </nav>
             </div>
-            
+
         </div>
         </>
     );
 }
- 
+
 export default Header;
+
+
+
+
