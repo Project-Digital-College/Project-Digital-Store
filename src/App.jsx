5bc6f59bf172686761d3e7e005789b00383ce884
Kaiky Xavier <kaikyxavier59@gmail.com>
2024-08-03 12:49:31 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Footer from "./components/Footer";
 import Header from "./components/Header";
+import Layout from "./components/Layout";
 
 const App = () => {
   return (
     <>
     <Header />
+    <Layout />
     <Footer />
     </>
   );
